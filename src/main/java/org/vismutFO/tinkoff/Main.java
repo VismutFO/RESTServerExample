13d1edb28654f4aa128f72c389ce0aeb53c8d2ab
VismutFO <p3lm2n1@mail.ru>
2023-10-30 09:23:14 +0300

    Checking commiting into main

--- src/main/java/org/vismutFO/tinkoff/Main.java
+++ src/main/java/org/vismutFO/tinkoff/Main.java
@@ -2,6 +2,6 @@ package org.vismutFO.tinkoff;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello REST!");
     }
 }
