9847c9cd4d21625ce99cd4444e3f567eb761ca33
VismutFO <p3lm2n1@mail.ru>
2023-10-31 18:37:55 +0300

    new init from https://start.spring.io/

--- src/main/java/org/vismutFO/tinkoff/Main.java
+++
@@ -1,7 +0,0 @@
-package org.vismutFO.tinkoff;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello REST!");
-    }
-}
---
+++ src/test/java/com/vismutFO/RESTservice/ResTserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vismutFO.RESTservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
